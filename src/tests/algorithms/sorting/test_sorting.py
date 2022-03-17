54659863ea934c8625153a7fb3a74ddacf9847ad
André Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-03-17 21:06:38 +0100

    added sorting tests

---
+++ src/tests/algorithms/sorting/test_sorting.py
@@ -0,0 +1,44 @@
+"""Tests the sorting implementations."""
+import copy
+import random
+import unittest
+from enum import Enum, auto
+from main.algorithms.sorting.bubble_sort import BubbleSort
+
+
+class SortingAlgorithm(Enum):
+    BUBBLE_SORT = auto()
+
+
+class TestSorting(unittest.TestCase):
+    """Class that tests sorting algorithms."""
+
+    def setUp(self) -> None:
+        self._algorithms = {SortingAlgorithm.BUBBLE_SORT: BubbleSort}
+
+    def test_small_positive_integers(self) -> None:
+        for size in range(0, 100):
+            for algorithm in self._algorithms:
+                sorter = self._algorithms.get(algorithm)()
+                values = self._generate_random_list(size, 0, 51)
+                cpy = copy.deepcopy(values)
+
+                values.sort()
+                sorter.sort_array(cpy)
+
+                self.assertEqual(values, cpy)
+
+    def test_small_negative_integers(self) -> None:
+        for size in range(0, 100):
+            for algorithm in self._algorithms:
+                sorter = self._algorithms.get(algorithm)()
+                values = self._generate_random_list(size, -50, 51)
+                cpy = copy.deepcopy(values)
+
+                values.sort()
+                sorter.sort_array(cpy)
+
+                self.assertEqual(values, cpy)
+
+    def _generate_random_list(self, size: int, low: int, high: int) -> list[int]:
+        return [random.randint(low, high) for _ in range(0, size)]
